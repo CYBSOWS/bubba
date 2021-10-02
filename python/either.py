9af5efef21c5cd9b90a6e351a06855b180139ce6
cybsows <cybsows@localhost>
2021-10-02 12:14:48 +0100

    Either: expose right/left values

--- python/either.py
+++ python/either.py
@@ -4,6 +4,22 @@ class Either:
         self.left = left
         self.isRight = isRight
 
+    def get_right(self):
+        if not isRight:
+            raise Exception('Either: guard call with Either#is_right')
+        return self.right
+
+    def get_left(self):
+        if isRight:
+            raise Exception('Either: guard call with Either#is_left')
+        return self.right
+
+    def is_right(self):
+        return self.isRight
+
+    def is_left(self):
+        return not self.isRight
+
     @staticmethod
     def left(left):
         return Either(None, left, False)
